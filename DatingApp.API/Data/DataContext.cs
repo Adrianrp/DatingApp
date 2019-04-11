74dc162febc275a102943ca9361b1ad1ff8e49fe
adrian <adrian@perdomo.dk>
2019-04-11 15:01:51 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        // Properties, we give it the type of Value, which is located in Models>Value.cs
+        // By convention we pluralize the name of the entities, and this is what is going to be called in the Db Table
+        public DbSet<Value> Values { get; set; }
+    }
+}
